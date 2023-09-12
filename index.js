8a53f5f0e6d79a1aa307a06459e59a98d6e1f996
+ <+>
2023-09-11 21:52:39 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
 
-// Write your code here
+
+const random = Math.round(Math.random() * 100) 
+
+
+let num3 = 28
+let num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20)
+
